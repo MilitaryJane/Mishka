629ba5261d7f815384269382f1778482c6f5d707
MilitaryJane <lusen_86@mail.ru>
2019-08-28 14:31:44 +0300

    Final

--- src/js/modal_add_basket_controls.js
+++ src/js/modal_add_basket_controls.js
@@ -6,20 +6,26 @@
     var modalAddBasket = document.querySelector('.modal-add-basket');
     var buttonAddBasket = document.querySelector('.modal-add-basket__button');
 
+    var buttonSizeSelect = document.querySelectorAll('.modal-add-basket__size-select');
+    buttonSizeSelect = Array.prototype.slice.call(buttonSizeSelect);
+    var arraySElectSize = [];
+    var basketIndicator = document.querySelector('.user-list__basket-indicator');
+
 
     buyButton.forEach(function (element) {
         element.addEventListener('click', function (evt) {
             evt.preventDefault();
             modalAddBasket.classList.add('modal-add-basket--open');
             window.overlayUtils.overlayShow();
+
         })
     });
 
 
     buttonAddBasket.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        basketIndicator.textContent = arraySElectSize.length;
         modalAddBasket.classList.remove('modal-add-basket--open');
         window.overlayUtils.overlayHide();
     });
-
-
 })();
