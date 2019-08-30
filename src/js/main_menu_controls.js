947606b49510ce274de7941162bcad6b3bf19150
MilitaryJane <lusen_86@mail.ru>
2019-08-30 19:30:13 +0300

    Update

--- src/js/main_menu_controls.js
+++ src/js/main_menu_controls.js
@@ -29,29 +29,3 @@
     });
 
 })();
-
-(function () {
-    'use strict';
-
-    var buyButton = document.querySelectorAll('.buy-button');
-    buyButton = Array.prototype.slice.call(buyButton);
-    var modalAddBasket = document.querySelector('.modal-add-basket');
-    var buttonAddBasket = document.querySelector('.modal-add-basket__button');
-
-
-    buyButton.forEach(function (element) {
-        element.addEventListener('click', function (evt) {
-            evt.preventDefault();
-            modalAddBasket.classList.add('modal-add-basket--open');
-            window.overlayUtils.overlayShow();
-        })
-    });
-
-
-    buttonAddBasket.addEventListener('click', function (evt) {
-        modalAddBasket.classList.remove('modal-add-basket--open');
-        window.overlayUtils.overlayHide();
-    });
-
-
-})();
